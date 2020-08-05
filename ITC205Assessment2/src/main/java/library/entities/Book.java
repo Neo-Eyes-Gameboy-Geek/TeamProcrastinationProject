ceb6dc62e225cef8d0648827da6d3073104fe35d
Dylan Sheaves <40333095+Neo-Eyes-Gameboy-Geek@users.noreply.github.com>
2020-08-05 15:46:01 +1000

    Replacing Book.Java
    Due to issues resulting from merge errors likely caused by directory changes etc. the modified book file has been moved to a new branch so as to make the merge easier on everyone.

--- ITC205Assessment2/src/main/java/library/entities/Book.java
+++ ITC205Assessment2/src/main/java/library/entities/Book.java
@@ -1,97 +1,88 @@
 package library.entities;
-import java.io.Serializable;
 
+import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Book implements Serializable {
-	
-	private String tItLe;
-	private String AuThOr;
-	private String CALLNO;
-	private int iD;
-	
-	private enum sTaTe { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
-	private sTaTe StAtE;
-	
-	
-	public Book(String author, String title, String callNo, int id) {
-		this.AuThOr = author;
-		this.tItLe = title;
-		this.CALLNO = callNo;
-		this.iD = id;
-		this.StAtE = sTaTe.AVAILABLE;
-	}
-	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(iD).append("\n")
-		  .append("  Title:  ").append(tItLe).append("\n")
-		  .append("  Author: ").append(AuThOr).append("\n")
-		  .append("  CallNo: ").append(CALLNO).append("\n")
-		  .append("  State:  ").append(StAtE);
-		
-		return sb.toString();
-	}
 
-	public Integer gEtId() {
-		return iD;
-	}
+    private String title;
+    private String author;
+    private String callNo;
+    private int id;
 
-	public String gEtTiTlE() {
-		return tItLe;
-	}
+    private enum State {
+        AVAILABLE, ON_LOAN, DAMAGED, RESERVED
+    };
+    private State state;
 
+    public Book(String author, String title, String callNo, int id) {
+        this.author = author;
+        this.title = title;
+        this.callNo = callNo;
+        this.id = id;
+        this.state = State.AVAILABLE;
+    }
 
-	
-	public boolean iS_AvAiLaBlE() {
-		return StAtE == sTaTe.AVAILABLE;
-	}
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Book: ").append(id).append("\n")
+                .append("  Title:  ").append(title).append("\n")
+                .append("  Author: ").append(author).append("\n")
+                .append("  CallNo: ").append(callNo).append("\n")
+                .append("  State:  ").append(state);
 
-	
-	public boolean iS_On_LoAn() {
-		return StAtE == sTaTe.ON_LOAN;
-	}
+        return sb.toString();
+    }
 
-	
-	public boolean iS_DaMaGeD() {
-		return StAtE == sTaTe.DAMAGED;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	
-	public void BoRrOw() {
-		if (StAtE.equals(sTaTe.AVAILABLE)) 
-			StAtE = sTaTe.ON_LOAN;
-		
-		else 
-			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", StAtE));
-		
-		
-	}
+    public String getTitle() {
+        return title;
+    }
 
+    public boolean isAvailable() {
+        return state == State.AVAILABLE;
+    }
 
-	public void ReTuRn(boolean DaMaGeD) {
-		if (StAtE.equals(sTaTe.ON_LOAN)) 
-			if (DaMaGeD) 
-				StAtE = sTaTe.DAMAGED;
-			
-			else 
-				StAtE = sTaTe.AVAILABLE;
-			
-		
-		else 
-			throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", StAtE));
-				
-	}
+    public boolean isOnLoan() {
+        return state == State.ON_LOAN;
+    }
 
-	
-	public void RePaIr() {
-		if (StAtE.equals(sTaTe.DAMAGED)) 
-			StAtE = sTaTe.AVAILABLE;
-		
-		else 
-			throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", StAtE));
-		
-	}
+    public boolean isDamaged() {
+        return state == State.DAMAGED;
+    }
 
+    public void borrowBook() {
+        if (state.equals(State.AVAILABLE)) {
+            state = State.ON_LOAN;
+        } else {
+            throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", state));
+        }
+
+    }
+
+    public void returnBook(boolean DaMaGeD) {
+        if (state.equals(State.ON_LOAN)) {
+            if (DaMaGeD) {
+                state = State.DAMAGED;
+            } else {
+                state = State.AVAILABLE;
+            }
+        } else {
+            throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", state));
+        }
+
+    }
+
+    public void repairBook() {
+        if (state.equals(State.DAMAGED)) {
+            state = State.AVAILABLE;
+        } else {
+            throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", state));
+        }
+
+    }
 
 }
