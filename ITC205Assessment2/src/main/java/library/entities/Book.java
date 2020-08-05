0ca57d8d564b03e2a690ea8c7979ba722b2df5ed
Dylan Sheaves <40333095+Neo-Eyes-Gameboy-Geek@users.noreply.github.com>
2020-08-05 17:18:49 +1000

    Reverting StringBuilder to try and fix git issues
    github apparently didnt see my changes last time so im redoing them

--- ITC205Assessment2/src/main/java/library/entities/Book.java
+++ ITC205Assessment2/src/main/java/library/entities/Book.java
@@ -24,14 +24,14 @@ public class Book implements Serializable {
     }
 
     public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Book: ").append(id).append("\n")
+        StringBuilder stringbuilder = new StringBuilder();
+        stringbuilder.append("Book: ").append(id).append("\n")
                 .append("  Title:  ").append(title).append("\n")
                 .append("  Author: ").append(author).append("\n")
                 .append("  CallNo: ").append(callNo).append("\n")
                 .append("  State:  ").append(state);
 
-        return stringBuilder.toString();
+        return stringbuilder.toString();
     }
 
     public Integer getId() {
