ee7bd8b8403fda01e7c739dd6832410f00a43795
Draco2000AR <trick17ar@gmail.com>
2020-08-19 15:42:02 +1000

    I had sent out and pushed 6 previous commits before this one, so this is a bump commit (it will be stated as a issue.)

--- ITC205Assessment2/src/main/java/library/fixbook/FixBookControl.java
+++ ITC205Assessment2/src/main/java/library/fixbook/FixBookControl.java
@@ -1,69 +1,75 @@
 package library.fixbook;
+
 import library.entities.Book;
 import library.entities.Library;
 
 public class FixBookControl {
-	
-	private FixBookUI uI;
-	private enum CoNtRoL_StAtE { INITIALISED, READY, FIXING };
-	private CoNtRoL_StAtE state;
-	
-	private Library library;
-	private Book CuRrEnT_BoOk;
 
-	public FixBookControl() {
-		this.library = Library.getInstance();
-		state = CoNtRoL_StAtE.INITIALISED;
-	}
+    private FixBookUI uI;
 
-	public void SeT_Ui(FixBookUI ui) {
-		if (!state.equals(CoNtRoL_StAtE.INITIALISED)) 
-			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
-			
-		this.uI = ui;
-		ui.setState(FixBookUI.UIState.READY);
-		state = CoNtRoL_StAtE.READY;		
-	}
+    private enum CoNtRoL_StAtE {
+        INITIALISED, READY, FIXING
+    };
+    private CoNtRoL_StAtE state;
 
+    private Library library;
+    private Book CuRrEnT_BoOk;
 
+    public FixBookControl() {
+        this.library = Library.getInstance();
+        state = CoNtRoL_StAtE.INITIALISED;
+    }
 
-	public void BoOk_ScAnNeD(int BoOkId) {
-		if (!state.equals(CoNtRoL_StAtE.READY)) 
-			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
-			
-		CuRrEnT_BoOk = library.getBook(BoOkId);
-		
-		if (CuRrEnT_BoOk == null) {
-			uI.display("Invalid bookId");
-			return;
-		}
-		if (!CuRrEnT_BoOk.isDamaged()) {
-			uI.display("Book has not been damaged");
-			return;
-		}
-		uI.display(CuRrEnT_BoOk.toString());
-		uI.setState(FixBookUI.UIState.FIXING);
-		state = CoNtRoL_StAtE.FIXING;		
-	}
+    public void SeT_Ui(FixBookUI ui) {
+        if (!state.equals(CoNtRoL_StAtE.INITIALISED)) {
+            throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
+        }
 
-	public void FiX_BoOk(boolean mUsT_FiX) {
-		if (!state.equals(CoNtRoL_StAtE.FIXING)) 
-			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
-			
-		if (mUsT_FiX) 
-			library.repairBook(CuRrEnT_BoOk);
-		
-		CuRrEnT_BoOk = null;
-		uI.setState(FixBookUI.UIState.READY);
-		state = CoNtRoL_StAtE.READY;		
-	}
+        this.uI = ui;
+        ui.setState(FixBookUI.UIState.READY);
+        state = CoNtRoL_StAtE.READY;
+    }
 
-	
-	public void SCannING_COMplete() {
-		if (!state.equals(CoNtRoL_StAtE.READY)) 
-			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
-			
-		uI.setState(FixBookUI.UIState.COMPLETED);
-	}
+    public void BoOk_ScAnNeD(int BoOkId) {
+        if (!state.equals(CoNtRoL_StAtE.READY)) {
+            throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
+        }
+
+        CuRrEnT_BoOk = library.getBook(BoOkId);
+
+        if (CuRrEnT_BoOk == null) {
+            uI.display("Invalid bookId");
+            return;
+        }
+        if (!CuRrEnT_BoOk.isDamaged()) {
+            uI.display("Book has not been damaged");
+            return;
+        }
+        uI.display(CuRrEnT_BoOk.toString());
+        uI.setState(FixBookUI.UIState.FIXING);
+        state = CoNtRoL_StAtE.FIXING;
+    }
+
+    public void FiX_BoOk(boolean mUsT_FiX) {
+        if (!state.equals(CoNtRoL_StAtE.FIXING)) {
+            throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
+        }
+
+        if (mUsT_FiX) {
+            library.repairBook(CuRrEnT_BoOk);
+        }
+
+        CuRrEnT_BoOk = null;
+        uI.setState(FixBookUI.UIState.READY);
+        state = CoNtRoL_StAtE.READY;
+    }
+
+    public void SCannING_COMplete() {
+        if (!state.equals(CoNtRoL_StAtE.READY)) {
+            throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
+        }
+
+        uI.setState(FixBookUI.UIState.COMPLETED);
+    }
 
 }
